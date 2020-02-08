e64ce6bf90fb19507ce56e7cf71432174ae383d1
fanrui <1996fanrui@gmail.com>
2020-02-09 00:14:23 +0800

    NewFeature：递归遍历二叉树 增加注释

--- module-algo/src/main/java/com/dream/tree/traversal/PreInPosRecursiveTraversal.java
+++ module-algo/src/main/java/com/dream/tree/traversal/PreInPosRecursiveTraversal.java
@@ -16,33 +16,42 @@ public class PreInPosRecursiveTraversal {
     }
 
     public static void preOrderRecur(Node head) {
+        System.out.print("先序遍历: ");
         if (head == null) {
             return;
         }
         // 遍历自己
         System.out.print(head.value + " ");
-        // 遍历左子树
+        // 递归遍历左子树
         preOrderRecur(head.left);
-        // 遍历右子树
+        // 递归遍历右子树
         preOrderRecur(head.right);
     }
 
     public static void inOrderRecur(Node head) {
+        System.out.print("中序遍历: ");
         if (head == null) {
             return;
         }
-        inOrderRecur(head.left);
+        // 递归遍历左子树
+        preOrderRecur(head.left);
+        // 遍历自己
         System.out.print(head.value + " ");
-        inOrderRecur(head.right);
+        // 递归遍历右子树
+        preOrderRecur(head.right);
     }
 
     public static void posOrderRecur(Node head) {
+        System.out.print("后序遍历: ");
         if (head == null) {
             return;
         }
-        posOrderRecur(head.left);
-        posOrderRecur(head.right);
+        // 递归遍历左子树
+        preOrderRecur(head.left);
+        // 遍历自己
         System.out.print(head.value + " ");
+        // 递归遍历右子树
+        preOrderRecur(head.right);
     }
 
 }
