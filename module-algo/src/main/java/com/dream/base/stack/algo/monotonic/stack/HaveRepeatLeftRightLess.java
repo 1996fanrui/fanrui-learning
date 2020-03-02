9f4eca5f1bbb1c974d063f680e4c439a717a3b23
fanrui <1996fanrui@gmail.com>
2020-03-02 23:54:11 +0800

    NewFeature： 单调栈的两个案例，有重复元素和无重复元素

--- module-algo/src/main/java/com/dream/base/stack/algo/monotonic/stack/HaveRepeatLeftRightLess.java
+++ module-algo/src/main/java/com/dream/base/stack/algo/monotonic/stack/HaveRepeatLeftRightLess.java
@@ -3,6 +3,7 @@ package com.dream.base.stack.algo.monotonic.stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -10,6 +11,7 @@ import java.util.Stack;
  * 给定一个可能含有重复值的数组 arr，找到每一个 i 位置左边和右边离 i 位置最近且值比 arr[i] 小的位置。返回所有位置相应的信息。
  * https://www.nowcoder.com/practice/2a2c00e7a88a498693568cef63a4b7bb
  *
+ * 如果有相同的元素，则先暂时放到 临时 list 中，最后一块处理这些相同元素
  */
 public class HaveRepeatLeftRightLess {
     /**
@@ -23,49 +25,48 @@ public class HaveRepeatLeftRightLess {
         int[][] ret = new int[arr.length][2];
         // 单调栈，栈底到栈顶，由小到大
         Stack<Integer> stack = new Stack<>();
-        int[] tmpList = new int[arr.length];
-        int tmpIndex = -1;
+        LinkedList<Integer> list = new LinkedList<>();
 
         for(int i = 0; i < arr.length; i++) {
             // 将栈中大于当前元素的全部弹出
             while(!stack.isEmpty() && arr[i] < arr[stack.peek()]){
                 int cur = stack.pop();
-                tmpList[++tmpIndex] = cur;
+                list.add(cur);
                 // 相同的元素先暂时放到 临时 list 中
                 while (!stack.isEmpty() && arr[cur] == arr[stack.peek()]){
-                    tmpList[++tmpIndex] = stack.pop();
+                    list.add(stack.pop());
                 }
 
                 // 临时栈中的所有元素相同，且 左右都一样
                 int left = stack.isEmpty() ? -1 : stack.peek();
                 do {
-                    cur = tmpList[tmpIndex--];
+                    cur = list.removeFirst();
                     // cur 右边小的最近的是 i
                     ret[cur][1] = i;
                     // cur 左边小的最近的是 null，或者栈中下一个元素
                     ret[cur][0] = left;
-                } while (tmpIndex >= 0);
+                } while (!list.isEmpty());
             }
             stack.push(i);
         }
 
         while(!stack.isEmpty()) {
             int cur = stack.pop();
-            tmpList[++tmpIndex] = cur;
+            list.add(cur);
             // 相同的元素先暂时放到 临时栈中
             while (!stack.isEmpty() && arr[cur] == arr[stack.peek()]){
-                tmpList[++tmpIndex] = stack.pop();
+                list.add(stack.pop());
             }
 
             // 临时栈中的所有元素相同，且 左右都一样
             int left = stack.isEmpty() ? -1 : stack.peek();
             do {
-                cur = tmpList[tmpIndex--];
+                cur = list.removeFirst();
                 // cur 右边小的最近的是 null
                 ret[cur][1] = -1;
                 // cur 左边小的最近的是 null，或者栈中下一个元素
                 ret[cur][0] = left;
-            } while (tmpIndex >= 0);
+            } while (!list.isEmpty());
         }
         return ret;
     }
@@ -76,11 +77,15 @@ public class HaveRepeatLeftRightLess {
         int[] arr = new int[n];
         String[] numbers = bufferedReader.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.valueOf(numbers[i]);
+            arr[i] = Integer.parseInt(numbers[i]);
         }
         int[][] res = getNearLess(arr);
-        for (int i = 0; i < res.length; i++) {
-            System.out.println(res[i][0] + " " + res[i][1]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < res.length-1; i++) {
+            sb.append(res[i][0]).append(" ").append(res[i][1]).append("\n");
         }
+        sb.append(res[res.length-1][0]).append(" ").append(res[res.length-1][1]);
+        System.out.println(sb.toString());
+        bufferedReader.close();
     }
 }
