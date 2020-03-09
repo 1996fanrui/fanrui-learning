8c99d62d68fe417d09beef29fe25885d516d05ec
fanrui <1996fanrui@gmail.com>
2020-03-10 01:21:09 +0800

    NewFeature：完善 二叉树的直径

--- module-algo/src/main/java/com/dream/tree/algo/recursion/MaxDistanceInTree.java
+++ module-algo/src/main/java/com/dream/tree/algo/recursion/MaxDistanceInTree.java
@@ -5,70 +5,70 @@ import java.io.*;
  * @author fanrui
  * 	二叉树节点间的最大距离问题
  * 	牛客链接：https://www.nowcoder.com/practice/88331be6da0d40749b068586dc0a2a8b?tpId=101&tqId=33247&tPage=1&rp=1&ru=/ta/programmer-code-interview-guide&qru=/ta/programmer-code-interview-guide/question-ranking
+ * 	LeetCode 543 二叉树的直径：https://leetcode-cn.com/problems/diameter-of-binary-tree/
  */
 public class MaxDistanceInTree {
 
-
-    public static class Node {
-        public int value;
-        public Node left;
-        public Node right;
-
-        public Node(int data) {
-            this.value = data;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
         }
+        return process(root).curDiameter - 1;
     }
 
-
-    private static class ReturnType {
-        public static final ReturnType NULL_RETURN = new ReturnType(0,0);
-        int height;
-        int maxDistance;
-
-        public ReturnType(int height, int maxDistance) {
-            this.height = height;
-            this.maxDistance = maxDistance;
-        }
-    }
-
-
-    public static int getMaxDistance(Node head) {
-        return process(head).maxDistance;
-    }
-
-
-    private static ReturnType process(Node head) {
-        if (head == null) {
+    private ReturnType process(TreeNode head){
+        if(null == head){
             return ReturnType.NULL_RETURN;
         }
 
-        ReturnType leftResult = process(head.left);
-        ReturnType rightResult = process(head.right);
+        // 计算左右子树 的 高度和直径
+        ReturnType leftRes = process(head.left);
+        ReturnType rightRes = process(head.right);
 
-        int curMaxDistance = leftResult.height + rightResult.height + 1;
-        curMaxDistance = Math.max(curMaxDistance,
-                Math.max(leftResult.maxDistance, rightResult.maxDistance));
+        int curDiameter = leftRes.height + rightRes.height + 1;
+        int curHeight = Math.max(leftRes.height, rightRes.height) + 1;
+        curDiameter = Math.max(curDiameter,
+                Math.max(leftRes.curDiameter, rightRes.curDiameter));
 
-        return new ReturnType(Math.max(leftResult.height, rightResult.height) + 1
-                , curMaxDistance);
+        return new ReturnType(curHeight, curDiameter);
+    }
+
+
+    public static class ReturnType{
+        int height;
+        int curDiameter;
+
+        public static ReturnType NULL_RETURN = new ReturnType(0,0);
+
+        public ReturnType(int height, int curDiameter) {
+            this.height = height;
+            this.curDiameter = curDiameter;
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
     }
 
 
     public static void main (String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
-        Node root = buildTree(br);
-        int res = getMaxDistance(root);
+        TreeNode root = buildTree(br);
+        int res = new MaxDistanceInTree().diameterOfBinaryTree(root);
         System.out.println(res);
     }
 
-    public static Node buildTree (BufferedReader br) throws IOException {
+    public static TreeNode buildTree (BufferedReader br) throws IOException {
         String[] str = br.readLine().split(" ");
         int[] nodes = new int[str.length];
         for (int i = 0; i < nodes.length; i++) {
             nodes[i] = Integer.parseInt(str[i]);
         }
-        Node root = new Node(nodes[0]);
+        TreeNode root = new TreeNode(nodes[0]);
         if (nodes[1] != 0) {
             root.left = buildTree(br);
         }
