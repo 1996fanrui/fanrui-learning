2a54acc7a374a3cb6b7b4d9826480f47771790e4
fanrui <1996fanrui@gmail.com>
2020-03-22 10:42:26 +0800

    NewFeature：完善 求树的高度 或者 最大深度

--- module-algo/src/main/java/com/dream/tree/algo/recursion/TreeHeight.java
+++ module-algo/src/main/java/com/dream/tree/algo/recursion/TreeHeight.java
@@ -4,6 +4,7 @@ package com.dream.tree.algo.recursion;
 /**
  * @author fanrui
  * LeetCode 104：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/submissions/
+ * 剑指 Offer 55-I： https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
  * 求树的高度 或者 最大深度
  * 递归遍历，左右子树，拿到左树高度，拿到右树高度。最大值 +1 即可
  */
@@ -19,8 +20,9 @@ public class TreeHeight {
         }
     }
 
-    public static int getTreeHeight(TreeNode root){
-        if(root == null){
+    // 思路一：递归遍历左子树和右子树，左右子树的最高高度 +1
+    public static int getTreeHeight(TreeNode root) {
+        if (root == null) {
             return 0;
         }
         // 拿到左树高度，拿到右树高度。最大值 +1
@@ -30,6 +32,26 @@ public class TreeHeight {
     }
 
 
+    int res = 0;
+
+    // 思路二：递归时，将 level 传递下去，遍历到 叶节点更新 MaxHeight
+    public int maxDepth(TreeNode root) {
+        res = 0;
+        if (root == null) {
+            return res;
+        }
+        preOrder(root, 1);
+        return res;
+    }
+
+    private void preOrder(TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+        res = Math.max(res, level);
+        preOrder(root.left, level + 1);
+        preOrder(root.right, level + 1);
+    }
 
 
 }
