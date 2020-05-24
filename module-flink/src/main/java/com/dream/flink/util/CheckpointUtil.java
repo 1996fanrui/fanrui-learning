c42e86cf589e1bd3eaba55765510c10a093c216a
fanrui03 <fanrui03@kuaishou.com>
2020-05-24 14:51:42 +0800

    Enhancement: 完善 CheckpointUtil 工具类：增加读取默认读取 flink-conf.yaml 文件的相关配置

--- module-flink/src/main/java/com/dream/flink/util/CheckpointUtil.java
+++ module-flink/src/main/java/com/dream/flink/util/CheckpointUtil.java
@@ -5,6 +5,7 @@ import org.apache.flink.contrib.streaming.state.PredefinedOptions;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -23,6 +24,7 @@ public class CheckpointUtil {
 
     /**
      * 设置状态后端为 RocksDBStateBackend
+     *
      * @param env env
      * @throws IOException
      */
@@ -41,42 +43,46 @@ public class CheckpointUtil {
         rocksDBStateBackend.setNumberOfTransferingThreads(NUMBER_OF_TRANSFER_THREADS);
         rocksDBStateBackend.setPredefinedOptions(PredefinedOptions.SPINNING_DISK_OPTIMIZED_HIGH_MEM);
         rocksDBStateBackend.enableTtlCompactionFilter();
-        env.setStateBackend((StateBackend)rocksDBStateBackend);
+        env.setStateBackend((StateBackend) rocksDBStateBackend);
     }
 
 
     /**
      * 设置状态后端为 FsStateBackend
+     *
      * @param env env
      * @throws IOException
      */
     public static void setFsStateBackend(StreamExecutionEnvironment env) throws IOException {
+        setConfYamlStateBackend(env);
+        FsStateBackend fsStateBackend = new FsStateBackend("hdfs:///user/flink/checkpoints");
+        env.setStateBackend((StateBackend) fsStateBackend);
+    }
 
+
+    /**
+     * 设置状态后端为 MemoryStateBackend
+     *
+     * @param env env
+     * @throws IOException
+     */
+    public static void setMemoryStateBackend(StreamExecutionEnvironment env) throws IOException {
+        setConfYamlStateBackend(env);
+        env.setStateBackend((StateBackend) new MemoryStateBackend());
+    }
+
+    /**
+     * Checkpoint 参数相关配置，but 不设置 StateBackend，即：读取 flink-conf.yaml 文件的配置
+     *
+     * @param env env
+     * @throws IOException
+     */
+    public static void setConfYamlStateBackend(StreamExecutionEnvironment env) throws IOException {
         env.enableCheckpointing(TimeUnit.MINUTES.toMillis(1));
 
         CheckpointConfig checkpointConf = env.getCheckpointConfig();
         checkpointConf.setMinPauseBetweenCheckpoints(TimeUnit.SECONDS.toMillis(50));
         checkpointConf.setCheckpointTimeout(TimeUnit.MINUTES.toMillis(3));
-        checkpointConf.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
-        checkpointConf.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
-
-        FsStateBackend fsStateBackend = new FsStateBackend("hdfs:///user/flink/checkpoints");
-        env.setStateBackend((StateBackend)fsStateBackend);
-    }
-
-
-    /**
-     * 设置状态后端为 FsStateBackend
-     * @param env env
-     * @throws IOException
-     */
-    public static void setMemoryStateBackend(StreamExecutionEnvironment env) throws IOException {
-
-        env.enableCheckpointing(TimeUnit.MINUTES.toMillis(1));
-
-        CheckpointConfig checkpointConf = env.getCheckpointConfig();
-        checkpointConf.setMinPauseBetweenCheckpoints(TimeUnit.SECONDS.toMillis(50));
-        checkpointConf.setCheckpointTimeout(TimeUnit.MINUTES.toMillis(2));
         checkpointConf.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         checkpointConf.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
