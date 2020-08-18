866a0bb8086bcaf67e9199cfb984ab5c795bf69e
fanrui03 <fanrui03@kuaishou.com>
2020-08-18 11:28:35 +0800

    flink 打包流程完成

--- module-flink/tools.sh
+++ module-flink/tools.sh
@@ -5,6 +5,7 @@ mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Drat.skip=tr
 
 mvn -T 8 clean install -DskipTests -Dmaven.javadoc.skip=true -Dcheckstyle.skip=true
 mvn -T 8 clean install -DskipTests -Dmaven.javadoc.skip=true -Drat.skip=true -Dcheckstyle.skip=true
+mvn -T 8 clean package -DskipTests -Dmaven.javadoc.skip=true -Drat.skip=true -Dcheckstyle.skip=true
 
 mvn -T 8 clean install -DskipTests -Dhadoop.version=2.7.6 -Dmaven.javadoc.skip=true -Drat.skip=true -Dcheckstyle.skip=true
 
@@ -32,7 +33,10 @@ mvn clean install -Dmaven.test.skip=true -Dhadoop.version=2.7.6 -Dmaven.javadoc.
 
 https://blog.csdn.net/baifanwudi/article/details/99564142
 
-
+flink 项目的 flink-dist/target/flink-1.10.0-XXX-bin 目录下
+执行 tar -zcvf flink-1.10.0-XXX.tar.gz flink-1.10.0-XXX
+将会打包好，然后将 flink-1.10.0-XXX.tar.gz 上传到集群
+tar -zxvf flink-1.10.0-XXX.tar.gz
 
 
 
