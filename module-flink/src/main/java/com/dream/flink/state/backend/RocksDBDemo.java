9fb62e9109df5930a47e24b9b0b410f64cc68737
fanrui03 <1996fanrui@gmail.com>
2020-11-25 19:25:04 +0800

    rocksdb demo

--- module-flink/src/main/java/com/dream/flink/state/backend/RocksDBDemo.java
+++ module-flink/src/main/java/com/dream/flink/state/backend/RocksDBDemo.java
@@ -1,5 +1,6 @@
 package com.dream.flink.state.backend;
 
+import org.rocksdb.Checkpoint;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
@@ -11,6 +12,7 @@ public class RocksDBDemo {
 
     private static byte[] KEY = "key".getBytes();
     private static String ROCKSDB_DIR = "/Users/fanrui03/Documents/tmp/rocksdb";
+    private static String ROCKSDB_CHECKPOINT_DIR = "/Users/fanrui03/Documents/tmp/rocksdb1";
 
     public static void main(String[] args) throws RocksDBException {
 
@@ -18,6 +20,10 @@ public class RocksDBDemo {
         db.compactRange();
         db.put(KEY, "value".getBytes());
         System.out.println(new String(db.get(KEY)));
+
+        Checkpoint checkpoint = Checkpoint.create(db);
+        checkpoint.createCheckpoint(ROCKSDB_CHECKPOINT_DIR);
+
 
     }
 
