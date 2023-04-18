5f7df1efaab23673a0cb51a76a83a5d912b18923
fanrui <1996fanrui@gmail.com>
2023-04-18 14:43:16 +0800

    测试 log4j2 的 JsonTemplateLayout

---
+++ module-java/src/main/java/com/dream/log/JsonTemplateLayoutTest.java
@@ -0,0 +1,35 @@
+package com.dream.log;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * refer:
+ * https://logging.apache.org/log4j/2.x/manual/json-template-layout.html
+ * https://logging.apache.org/log4j/2.x/manual/lookups.html
+ *
+ * <dependency>
+ *     <groupId>org.apache.logging.log4j</groupId>
+ *     <artifactId>log4j-layout-template-json</artifactId>
+ *     <version>2.20.0</version>
+ * </dependency>
+ *
+ * appender.console.json.type = JsonTemplateLayout
+ * appender.console.json.eventTemplateUri = classpath:EcsLayout.json
+ *
+ * EcsLayout.json 中可以使用 "${env:USER}" 打印环境变量。
+ */
+public class JsonTemplateLayoutTest {
+
+    protected static final Logger LOG = LoggerFactory.getLogger(JsonTemplateLayoutTest.class);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 10; i++) {
+            LOG.info("Test for json layout, {}", i);
+            Thread.sleep(1000);
+        }
+    }
+}
