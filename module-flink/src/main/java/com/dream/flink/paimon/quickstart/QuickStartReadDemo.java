c7545d0c37820e7014b78bb3e2b02b348ac7d3b4
fanrui <1996fanrui@gmail.com>
2023-06-24 21:36:15 +0800

    Add the quick start about reading and writing paimon

---
+++ module-flink/src/main/java/com/dream/flink/paimon/quickstart/QuickStartReadDemo.java
@@ -0,0 +1,51 @@
+package com.dream.flink.paimon.quickstart;
+
+import com.dream.flink.sql.FlinkSqlUtil;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import java.util.concurrent.TimeUnit;
+
+public class QuickStartReadDemo {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.setString("state.checkpoint-storage", "filesystem");
+        conf.setString("state.checkpoints.dir", "file:///tmp/flinkjob");
+//        conf.setString("execution.savepoint.path", "file:///tmp/flinkjob/d043c87439e832ccd1cadbf7ab5c6fe2/chk-43");
+        conf.set(RestOptions.PORT, 34568);
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        StreamTableEnvironment tableEnv = FlinkSqlUtil.getTableEnv(env);
+
+        env.enableCheckpointing(TimeUnit.SECONDS.toMillis(30));
+        env.setParallelism(1);
+
+        String paimonDDL = "CREATE TABLE word_count (\n" +
+                "    word STRING PRIMARY KEY NOT ENFORCED,\n" +
+                "    cnt BIGINT\n" +
+                ") with (\n" +
+                "    'connector' = 'paimon',\n" +
+                "    'path' = 'file:/tmp/paimon/default.db/word_count',\n" +
+                "    'auto-create' = 'true'\n" +
+                ")";
+
+        String printDDL = "CREATE TEMPORARY TABLE print_table (\n" +
+                "    word STRING,\n" +
+                "    cnt BIGINT\n" +
+                ") WITH (\n" +
+                "    'connector' = 'print'\n" +
+                ")";
+
+        String insertSQL = "INSERT INTO print_table SELECT * FROM word_count "
+//               + "/*+ OPTIONS('consumer-id' = 'myid') */"
+                ;
+
+        tableEnv.executeSql(paimonDDL);
+        tableEnv.executeSql(printDDL);
+        tableEnv.executeSql(insertSQL);
+    }
+
+}
---
+++ module-flink/src/main/java/com/dream/flink/paimon/quickstart/QuickStartWriteDemo.java
@@ -0,0 +1,56 @@
+package com.dream.flink.paimon.quickstart;
+
+import com.dream.flink.sql.FlinkSqlUtil;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestOptions;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import java.util.concurrent.TimeUnit;
+
+public class QuickStartWriteDemo {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.setString("state.checkpoint-storage", "filesystem");
+        conf.setString("state.checkpoints.dir", "file:///tmp/flinkjob");
+        conf.set(RestOptions.PORT, 34567);
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        StreamTableEnvironment tableEnv = FlinkSqlUtil.getTableEnv(env);
+
+        env.enableCheckpointing(TimeUnit.SECONDS.toMillis(10));
+        env.setParallelism(2);
+
+        // Don't use the catalog to prevent the table is exist.
+//        String catalogDDL = "CREATE CATALOG my_catalog WITH (\n" +
+//                "    'type'='paimon',\n" +
+//                "    'warehouse'='file:/tmp/paimon'\n" +
+//                ")";
+//
+//        String useCatalog = "USE CATALOG my_catalog";
+
+        String paimonDDL = "CREATE TABLE word_count (\n" +
+                "    word STRING PRIMARY KEY NOT ENFORCED,\n" +
+                "    cnt BIGINT\n" +
+                ") with (\n" +
+                "    'connector' = 'paimon',\n" +
+                "    'path' = 'file:/tmp/paimon/default.db/word_count',\n" +
+                "    'auto-create' = 'true'\n" +
+                ")";
+
+        String sourceDDL = "CREATE TEMPORARY TABLE word_table (\n" +
+                "    word STRING\n" +
+                ") WITH (\n" +
+                "    'connector' = 'datagen',\n" +
+                "    'fields.word.length' = '1'\n" +
+                ")";
+
+        String insertSQL = "INSERT INTO word_count SELECT word, COUNT(*) FROM word_table GROUP BY word\n";
+
+        tableEnv.executeSql(paimonDDL);
+        tableEnv.executeSql(sourceDDL);
+        tableEnv.executeSql(insertSQL);
+    }
+
+}
